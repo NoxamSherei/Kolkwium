1ae1f5ef9509f651675239e28a1d36af61a72444
user <user@USER-KOMPUTER>
2017-06-19 11:37:52 +0200

    Wprowadzenie pobioru danych x, y do obliczen kąta

--- KolokwiumKaty/KolokwiumKaty/Program.cs
+++ KolokwiumKaty/KolokwiumKaty/Program.cs
@@ -10,6 +10,28 @@ namespace KolokwiumKaty
     {
         static void Main(string[] args)
         {
+            ModulLiczacy liczydlo = new ModulLiczacy();
+            bool ifWhile = false;
+            string colect;
+            Console.Write("prosze podac x: ");
+            colect = Console.ReadLine();
+            int x = Convert.ToInt32(colect);
+            Console.Write("prosze podac y: ");
+            colect = Console.ReadLine();
+            int y = Convert.ToInt32(colect);
+            float otrzymanyKatAlpha= liczydlo.Kat(x, y);
+            Console.WriteLine(otrzymanyKatAlpha);
+            Console.ReadKey();
+        }
+        
+    }
+    class ModulLiczacy
+    {
+        public float Kat(int x, int y)
+        {
+            float kat = 0;
+            kat = x / y;
+            return kat;
         }
     }
 }
